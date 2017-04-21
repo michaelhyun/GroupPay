00401bb362736dfa80518a72e449477bb5e4ba29
michaelhyun <michael.hyun3@gmail.com>
2017-04-21 16:09:51 -0700

    Created Initial Login View Controller with only UI and updated README

---
+++ GroupPay/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  GroupPay
+//
+//  Created by Michael Hyun on 4/18/17.
+//  Copyright © 2017 CMPE137. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var _username: UITextField!
+    @IBOutlet weak var _password: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+
+    }
+
+
+
+    @IBAction func LoginButton(_ sender: Any) {
+    }
+}
+
+
+
+
