45cc250bec134840000305dd51056151ab9ef2f6
michaelhyun <michael.hyun3@gmail.com>
2017-04-22 02:05:25 -0700

    Changed Log In and Sign Up View

--- GroupPay/SignUpViewController.swift
+++ GroupPay/SignUpViewController.swift
@@ -9,20 +9,34 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var _lastname: UITextField!
+    @IBOutlet weak var _firstname: UITextField!
+    @IBOutlet weak var _username: UITextField!
+    
+    @IBOutlet weak var _password: UITextField!
+    @IBOutlet weak var _email: UITextField!
+    
+    @IBAction func registerButtonTapped(_ sender: Any) {
+        let userEmail = _email.text
+        let userPass = _password.text
+        let userName = _username.text
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-       override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let nvc = segue.destination as? UINavigationController{
             if let homeVC = nvc.visibleViewController as? HomeViewController{
                 //send data
@@ -30,5 +44,5 @@ class SignUpViewController: UIViewController {
         }
     }
     
- 
+    
 }
