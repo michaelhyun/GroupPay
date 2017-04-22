44d2b2851dbae92c0192ad2429c1083bc6356edc
michaelhyun <michael.hyun3@gmail.com>
2017-04-22 00:18:37 -0700

    Fixed Back buttons for all Views, and commented out prepareForSegue in HomeVC

--- GroupPay/HomeViewController.swift
+++ GroupPay/HomeViewController.swift
@@ -30,34 +30,34 @@ class HomeViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
         let identifier = segue.identifier
-        switch identifier! {
-            case "Account":
-                if let accountVC = segue.destination as? AccountViewController{
-                    //data send
-                }
-            case "Group":
-                if let groupVC = segue.destination as? GroupsViewController{
-                    //data send
-                }
-            case "Events":
-                if let eventsVC = segue.destination as? EventsViewController{
-                    //data send
-                }
-            case "History":
-                if let historyVC = segue.destination as? HistoryViewController{
-                    //data send
-                }
-            case "Settings":
-                if let settingsVC = segue.destination as? SettingsViewController{
-                    //data send
-                }
-            case "Reminders":
-                if let remindersVC = segue.destination as? RemindersViewController{
-                    //data send
-                }
-            default:
-                return
-        }
+//        switch identifier! {
+//            case "Account":
+//                if let accountVC = segue.destination as? AccountViewController{
+//                    //data send
+//                }
+//            case "Group":
+//                if let groupVC = segue.destination as? GroupsViewController{
+//                    //data send
+//                }
+//            case "Events":
+//                if let eventsVC = segue.destination as? EventsViewController{
+//                    //data send
+//                }
+//            case "History":
+//                if let historyVC = segue.destination as? HistoryViewController{
+//                    //data send
+//                }
+//            case "Settings":
+//                if let settingsVC = segue.destination as? SettingsViewController{
+//                    //data send
+//                }
+//            case "Reminders":
+//                if let remindersVC = segue.destination as? RemindersViewController{
+//                    //data send
+//                }
+//            default:
+//                return
+//        }
     }
  
 
